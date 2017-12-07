a60925ea7ab19f79643fa0e252e21ea0e5f14425
Idayat <github email address>
2017-12-06 18:12:40 -0800

    add form for mesages

--- server/server.js
+++ server/server.js
@@ -1,12 +1,11 @@
 const path = require('path');
-const express = require('express');
 const http = require('http');
+const express = require('express');
 const socketIO = require('socket.io');
 
+const {generateMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
-
-
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
@@ -14,43 +13,28 @@ var io = socketIO(server);
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
-  console.log('New User connected');
+  console.log('New user connected');
 
-  socket.emit('newMessage', {
-    from: 'Admin',
-    text: 'Welcome to the Chat App'
-  });
+  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
 
-  socket.broadcast.emit('newMessage', {
-    from: "Admin",
-    text: "New User Joined",
-    createdAT: new Date().getTime()
-  });
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
-  socket.on('createMessage', (message) => {
-    console.log('createmessage', message);
-    io.emit('newMessage', {
-      from: message.from,
-      text: message.text,
-      createdAT: new Date().getTime()
-    });
-
+  socket.on('createMessage', (message, callback) => {
+    console.log('createMessage', message);
+    io.emit('newMessage', generateMessage(message.from, message.text));
+    callback('This is from the server.');
     // socket.broadcast.emit('newMessage', {
     //   from: message.from,
     //   text: message.text,
-    //   createdAT: new Date().getTime()
-    //
+    //   createdAt: new Date().getTime()
     // });
   });
 
-
-
-  socket.on('disconnet', () => {
-    console.log('User was Disconnected');
+  socket.on('disconnect', () => {
+    console.log('User was disconnected');
   });
 });
 
-
-server.listen(port, ()=> {
-  console.log(`Starting on ${port}`);
+server.listen(port, () => {
+  console.log(`Server is up on ${port}`);
 });
